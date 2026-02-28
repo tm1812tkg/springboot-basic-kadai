d2412c57cab845da333320a2bbc8c7de536b342c
tm1812tkg <tkn1812.p128@outlook.jp>
2026-02-28 21:54:21 +0900

    form課題を修正２回目

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,10 +1,13 @@
 package com.example.springkadaiform.controller;
 
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
@@ -12,20 +15,33 @@ import com.example.springkadaiform.form.ContactForm;
 public class ContactFormController {
 
 	@GetMapping("/form")
-	public String showForm(ContactForm contactForm) {
+	public String showForm(Model model) {
+		if (!model.containsAttribute("contactForm")) {
+            // ビューにフォームクラスのインスタンスを渡す
+            model.addAttribute("contactForm", new ContactForm());
+        }
 		return "contactFormView";
 	}
 
 	@PostMapping("/confirm")
 	public String confirm(
+			Model model,
 			@Validated ContactForm contactForm,
-			BindingResult bindingResult) {
+			BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
 
 		// バリデーションNG → フォームに戻す
 		if (bindingResult.hasErrors()) {
-			return "contactFormView";
-		}
+			redirectAttributes.addFlashAttribute("contactForm", contactForm);
+			// バリデーション結果をビューに受け渡す
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+					+ Conventions.getVariableName(contactForm), bindingResult);
 
+			return "redirect:/form";
+
+		}
+		
+		model.addAttribute("contactForm", contactForm);
 		// OK → 確認画面へ
 		return "confirmView";
 	}
